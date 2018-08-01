c3d629cbb4a94350aacdc7c9c0e63aecc633d1e7
adityastic <adityaofficialgupta@gmail.com>
2018-08-01 10:19:53 +0530

    Renamed Professor Files properly

--- application/controllers/Professors.php
+++ application/controllers/Professors.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class professors1 extends MY_Controller {
+class professors extends MY_Controller {
 
 	public function all_professor()
 	{
