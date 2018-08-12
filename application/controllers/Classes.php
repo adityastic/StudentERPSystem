7d1f1e3a3e149b6c197445b8885c2a65e4e780be
adityastic <adityaofficialgupta@gmail.com>
2018-08-12 14:47:04 +0530

    Updated Classes

--- application/controllers/Classes.php
+++ application/controllers/Classes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Classroom extends MY_Controller {
+class Classes extends MY_Controller {
 
 	public function index()
 	{
