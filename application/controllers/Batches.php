f309f74df2c02494540d2aba74d3c3d52d0c2088
adityastic <adityaofficialgupta@gmail.com>
2018-08-17 01:38:09 +0530

    Batches Controller, Added POST Check and some other basic stuff

--- application/controllers/Batches.php
+++ application/controllers/Batches.php
@@ -1,10 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Batches extends MY_Controller {
+class Batches extends MY_Controller
+{
+    public function add_batch()
+    {
+        $this->load->model('batches_model', '_batchmodel');
 
-	public function add_batch()
-	{
-		$this->load->view('add-courses');
-	}
+        if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $arr = $this->input->post('teachers');
+        }
+        $data['classes'] = $this->_batchmodel->get_all_classes();
+        $data['years'] =$this->_batchmodel->get_all_years();
+        $data['sections'] =$this->_batchmodel->get_all_sections();
+        $data['allteachers'] =$this->_batchmodel->get_all_teachers();
+        $data['allsubjects'] =$this->_batchmodel->get_all_subjects();
+        $this->load->view('batches-add', $data);
+    }
 }
