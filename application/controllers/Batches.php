71ec22381fdf7d02b89a6bb016ea3e64863b6cfd
adityastic <adityaofficialgupta@gmail.com>
2018-08-18 18:53:53 +0530

    Model Changed with Data for Batch

--- application/controllers/Batches.php
+++ application/controllers/Batches.php
@@ -5,6 +5,18 @@ class Batches extends MY_Controller {
 
 	public function add_batch()
 	{
-		$this->load->view('batches-add');
+		$this->load->model('Batches_model','_batchmodel');
+
+		if ($this->input->server('REQUEST_METHOD') == 'POST') {
+			$this->_batchmodel->checkBatch($this->input->post('field_class_id'), $this->input->post('field_year_id'), $this->input->post('field_section_id'));
+        }else{
+
+		$data['allProfs'] = $this->_batchmodel->get_all_teachers();
+		$data['allSubjects'] = $this->_batchmodel->get_all_subjects();
+        $data['classes'] = $this->_batchmodel->get_all_classes();
+        $data['years'] =$this->_batchmodel->get_all_years();
+        $data['sections'] =$this->_batchmodel->get_all_sections();
+		$this->load->view('batches-add',$data);
+		}
 	}
 }
