453c7a60d365900d165f6368d76ab0261d5dbf2a
adityastic <adityaofficialgupta@gmail.com>
2018-08-28 22:34:33 +0530

    Improved Controller for Basic Functionality of Update Students

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -124,7 +124,7 @@ class Students extends MY_Controller
             }
 
             if (!isset($errors->has)) {
-                if(!$this->_student->checkBatch($field_class_id, $field_year_id, $field_section_id)){
+                if (!$this->_student->checkBatch($field_class_id, $field_year_id, $field_section_id)) {
                     $errors = new stdClass();
                     $errors->has = true;
                     $this->setErrorMessage($errors, "- Batch not found, <a href=" . site_url('batches/add_batch') . ">Create a batch first</a>");
@@ -191,14 +191,15 @@ class Students extends MY_Controller
                     'mother_o_number'=>$field_mother_onumber,
                     'password'=>$password,
                 );
-                $this->_student->insertintoadmission($insertArray,$field_class_id,$field_year_id,$field_section_id);
+                $this->_student->insertintoadmission($insertArray, $field_class_id, $field_year_id, $field_section_id);
                 $sessarr = array(
                     'add_done' => 'yes',
                 );
                 $this->session->set_userdata($sessarr);
                 redirect('/students/add_students', 'refresh');
             }
-        }else
+        }
+
         if (!empty($this->session->userdata('add_done'))) {
             $this->session->sess_destroy();
             $data['done'] = true;
@@ -230,9 +231,14 @@ class Students extends MY_Controller
         $this->load->model('Student_all_model', '_student');
         $data['details']=$this->_student->get_all_students();
         $data['details1']=$this->_student->get_years();
+
+        if (!empty($this->session->userdata('add_student'))) {
+            $this->session->sess_destroy();
+            $data['done'] = true;
+        }
+
         $this->load->view('students-all', $data);
     }
-
 
     public function show_profile($id)
     {
@@ -250,71 +256,15 @@ class Students extends MY_Controller
             $this->load->view('students-profile', $data);
         }
     }
-    public function edit_profile()
+
+    public function update_student($id)
     {
-        $this->load->model('Student_edit_model', '_student');
-        $data['details']=$this->_student->get_all_students();
-
-        $this->load->view('students-edit', $data);
-    }
-    public function update_profile($id)
-    {
-        if (!empty($id)) {
-            $this->load->model('Student_profile_model', '_student');
-            $this->load->model('student_admission_model', '_student1');
-
-
-            $result = $this->_student->get_profile($id);
-
-            $data['name'] = $result->stud_name;
-            $data['current_address'] = $result->current_address;
-            $data['stud_email'] = $result->stud_email;
-            $data['stud_phone'] = $result->stud_phone;
-            $data['photo']=$result->photo;
-            $data['father_name']=$result->father_name;
-            $data['mother_name']=$result->mother_name;
-            $data['father_no']=$result->father_number;
-            $data['mother_no']=$result->mother_number;
-            $data['perma_address']=$result->perma_address;
-            $data['father_email']=$result->father_email;
-            $data['student_dob']  = DateTime::createFromFormat('Y-m-d', $result->stud_dob)->format('d/m/Y');
-            $data['student_gender']=$result->stud_gender;
-            $data['student_bg']=$result->stud_blood_group;
-            $data['student_caste']=$result->stud_caste;
-            $data['city']=$result->city;
-            $data['state']=$result->state;
-            $data['perma_address_pin']=$result->perma_address_pin;
-            $data['father_office_no']=$result->father_o_number;
-            $data['mother_office_no']=$result->mother_o_number;
-            $data['stud_password']=$result->password;
-            $data['current_address_pin']=$result->currrent_address_pin;
-            $data['admissionnumber'] = $id;
-
-            $Batch_result = $this->_student->get_batch($result->batch_id);
-            $data['batch']=$Batch_result;
-            $data['batch1']=$result->batch_id;
-            foreach ($Batch_result->result() as $row)
-            {
-                $data['year1'] = $row->year;
-                $data['class1'] = $row->class;
-                $data['section1'] = $row->section;
-            }
-
-            $data['classes'] = $this->_student1->get_all_classes();
-            $data['years'] =$this->_student1->get_all_years();
-            $data['sections'] =$this->_student1->get_all_sections();
-            $this->load->view('students-update', $data);
-        }
-    }
-    public function update_students($id1)
-    {
-        $this->load->model('student_admission_model', '_student');
-        $this->load->model('Student_profile_model', '_student1');
-
-        $data['admissionnumber'] =$id1;
-        $data['error_list'] = array();
+        $this->load->model('student_update_model', '_studentupdate');
 
         if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $this->load->model('student_admission_model', '_student');
+            $this->load->model('student_update_model', '_studentupdate');
+
             $field_photo = '';
 
             $config['upload_path'] = './uploads/';
@@ -326,8 +276,9 @@ class Students extends MY_Controller
             //VALIDATION CODE
             $field_adm_number = $this->input->post('field_adm_number');
             $field_stud_ph = $this->input->post('field_stud_ph');
-
+           
             $date1 = $this->input->post('field_stud_dob');
+
             $field_stud_dob  = DateTime::createFromFormat('d/m/Y', $date1)->format('Y-m-d');
             $field_city = $this->input->post('field_city');
             $field_curradd = $this->input->post('field_curradd');
@@ -347,7 +298,14 @@ class Students extends MY_Controller
                 $field_stud_fname = "";
             }
 
-            $student_name = $field_stud_fname;
+            $field_stud_lname = $this->input->post('field_stud_lname');
+            if ($this->checkNumbersinString($field_stud_lname)) {
+                $errors->has = true;
+                $this->setErrorMessage($errors, "- Enter Last Name without numbers");
+                $field_stud_lname="";
+            }
+
+            $student_name = $field_stud_fname . " " . $field_stud_lname;
 
             $field_stud_gender = $this->input->post('field_stud_gender');
             if (strcmp($field_stud_gender, 'none') == 0) {
@@ -423,7 +381,7 @@ class Students extends MY_Controller
             }
 
             if (!isset($errors->has)) {
-                if(!$this->_student->checkBatch($field_class_id, $field_year_id, $field_section_id)){
+                if (!$this->_student->checkBatch($field_class_id, $field_year_id, $field_section_id)) {
                     $errors = new stdClass();
                     $errors->has = true;
                     $this->setErrorMessage($errors, "- Batch not found, <a href=" . site_url('batches/add_batch') . ">Create a batch first</a>");
@@ -433,65 +391,42 @@ class Students extends MY_Controller
 
             if (isset($errors->has)) {
                 $data['errorstring'] = $errors->msg;
-                $modifiedArray = array(
-                    'field_stud_fname'=>$field_stud_fname,
-                    'field_father_name'=>$field_father_name,
-                    'field_mother_name'=>$field_mother_name,
-                    'field_father_number'=>$field_father_number,
-                    'field_mother_number'=>$field_mother_number,
-                    'field_curradd'=>$field_curradd,
-                    'field_permadd'=>$field_permadd,
-                    'field_father_email'=>$field_father_email,
-                    'field_stud_dob'=>$date1,
-                    'field_stud_gender'=>$field_stud_gender,
-                    'field_bgroup'=>$field_bgroup,
-                    'field_stud_caste'=>$field_stud_caste,
-                    'field_city'=>$field_city,
-                    'field_state'=>$field_state,
-                    'field_permpin'=>$field_permpin,
-                    'field_currpin'=>$field_currpin,
-                    'field_stud_email'=>$field_stud_email,
-                    'field_stud_ph'=>$field_stud_ph,
-                    'field_father_onumber'=>$field_father_onumber,
-                    'field_mother_onumber'=>$field_mother_onumber,
-                    'field_class_id'=>$field_class_id,
-                    'field_year_id'=>$field_year_id,
-                    'field_section_id'=>$field_section_id
-                );
-                $data['_reEntry'] = $modifiedArray;
+
+                $data['name'] = $field_stud_fname;
+                $data['current_address'] = $field_curradd;
+                $data['stud_email'] = $field_stud_email;
+                $data['stud_phone'] = $field_stud_ph;
+                $data['photo']=$result->photo;
+                $data['father_name']=$field_father_name;
+                $data['mother_name']=$field_mother_name;
+                $data['father_no']=$field_father_number;
+                $data['mother_no']=$field_mother_number;
+                $data['perma_address']=$field_permadd;
+                $data['father_email']=$field_father_email;
+                $data['student_dob']  = $date1;
+                $data['student_gender']=$field_stud_gender;
+                $data['student_bg']=$field_bgroup;
+                $data['student_caste']=$field_stud_caste;
+                $data['city']=$field_city;
+                $data['state']=$field_state;
+                $data['perma_address_pin']=$field_permpin;
+                $data['father_office_no']=$field_father_onumber;
+                $data['mother_office_no']=$field_mother_onumber;
+                $data['stud_password']=$result->password;
+                $data['current_address_pin']=$field_currpin;
+                $data['admissionnumber'] = $id;
+
+                $data['old_batch_id'] = $this->input->post('old_batch_id');
+                $data['yearSelected'] = $batch_content->year;
+                $data['classSelected'] = $batch_content->class;
+                $data['sectionSelected'] = $batch_content->section;
+
+                $data['classes'] = $field_class_id;
+                $data['years'] =$field_year_id;
+                $data['sections'] =$field_section_id;
+                $this->load->view('students-update', $data);
             } else {
-                date_default_timezone_set('Asia/Kolkata');
-                $date = date('d.m.Y H:i:s', time());
-                $current_date  = DateTime::createFromFormat('d.m.Y H:i:s', $date)->format('Y-m-d h:i:s');
-                if(!isset($field_photo))
-                {
-                    $insertArray = array(
-                    'adm_date'=>$current_date,
-                    'stud_name'=>$student_name,
-                    'father_name'=>$field_father_name,
-                    'mother_name'=>$field_mother_name,
-                    'father_number'=>$field_father_number,
-                    'mother_number'=>$field_mother_number,
-                    'current_address'=>$field_curradd,
-                    'perma_address'=>$field_permadd,
-                    'father_email'=>$field_father_email,
-                    'stud_dob'=>$field_stud_dob,
-                    'stud_gender'=>$field_stud_gender,
-                    'stud_blood_group'=>$field_bgroup,
-                    'stud_caste'=>$field_stud_caste,
-                    'city'=>$field_city,
-                    'state'=>$field_state,
-                    'perma_address_pin'=>$field_permpin,
-                    'currrent_address_pin'=>$field_currpin,
-                    'stud_email'=>$field_stud_email,
-                    'stud_phone'=>$field_stud_ph,
-                    'father_o_number'=>$field_father_onumber,
-                    'mother_o_number'=>$field_mother_onumber,
-                    'password'=>$password,
-                );
-                }else{
-                    $insertArray = array(
-                    'adm_date'=>$current_date,
+                $insertArray = array(
                     'stud_name'=>$student_name,
                     'father_name'=>$field_father_name,
                     'mother_name'=>$field_mother_name,
@@ -514,20 +449,54 @@ class Students extends MY_Controller
                     'father_o_number'=>$field_father_onumber,
                     'mother_o_number'=>$field_mother_onumber,
                     'password'=>$password,
-                );}
+                );
 
-                $this->_student1->update_student($id1,$insertArray,$field_class_id,$field_year_id,$field_section_id);
-
+                $this->_studentupdate->updateStudent($this->input->post('old_batch_id'), $insertArray, $id, $field_class_id, $field_year_id, $field_section_id);
+                $sessarr = array(
+                    'add_student' => 'yes',
+                );
+                $this->session->set_userdata($sessarr);
+                redirect('/students/all_students', 'refresh');
             }
-        }else
-        if (!empty($this->session->userdata('add_done'))) {
-            $this->session->sess_destroy();
-            $data['done'] = true;
-        }
+        } else {
+            $this->load->model('student_update_model', '_studentupdate');
+            $this->load->model('student_admission_model', '_student_adm');
 
-        $data['classes'] = $this->_student->get_all_classes();
-        $data['years'] =$this->_student->get_all_years();
-        $data['sections'] =$this->_student->get_all_sections();
-        $this->load->view('dashboard');
+            $result = $this->_studentupdate->get_profile($id);
+            $data['name'] = $result->stud_name;
+            $data['current_address'] = $result->current_address;
+            $data['stud_email'] = $result->stud_email;
+            $data['stud_phone'] = $result->stud_phone;
+            $data['photo']=$result->photo;
+            $data['father_name']=$result->father_name;
+            $data['mother_name']=$result->mother_name;
+            $data['father_no']=$result->father_number;
+            $data['mother_no']=$result->mother_number;
+            $data['perma_address']=$result->perma_address;
+            $data['father_email']=$result->father_email;
+            $data['student_dob']  = DateTime::createFromFormat('Y-m-d', $result->stud_dob)->format('d/m/Y');
+            $data['student_gender']=$result->stud_gender;
+            $data['student_bg']=$result->stud_blood_group;
+            $data['student_caste']=$result->stud_caste;
+            $data['city']=$result->city;
+            $data['state']=$result->state;
+            $data['perma_address_pin']=$result->perma_address_pin;
+            $data['father_office_no']=$result->father_o_number;
+            $data['mother_office_no']=$result->mother_o_number;
+            $data['stud_password']=$result->password;
+            $data['current_address_pin']=$result->currrent_address_pin;
+            $data['admissionnumber'] = $id;
+
+            $data['old_batch_id'] = $result->batch_id;
+            $batch_content = $this->_studentupdate->get_batch($result->batch_id)->row();
+            $data['yearSelected'] = $batch_content->year;
+            $data['classSelected'] = $batch_content->class;
+            $data['sectionSelected'] = $batch_content->section;
+
+            $data['classes'] = $this->_student_adm->get_all_classes();
+            $data['years'] =$this->_student_adm->get_all_years();
+            $data['sections'] =$this->_student_adm->get_all_sections();
+            $this->load->view('students-update', $data);
+        }
     }
 }
